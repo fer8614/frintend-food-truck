a54c56c3606a9169f30232e3c0060a0360b61684
Fernando <fernando.inversionmf@gmail.com>
2025-02-06 22:06:03 -0800

    config seeding and data fetching in prisma

--- components/0rderSiteBar.tsx
+++ components/0rderSiteBar.tsx
@@ -1,3 +1,12 @@
-export default function OrderSiteBar() {
+import { prisma } from "@/src/lib/prisma";
+
+async function getCategories() {
+  return prisma.category.findMany();
+}
+
+export default async function OrderSiteBar() {
+  const categories = await getCategories();
+  console.log(categories);
+
   return <aside className="md:w-72 md:h-screen bg-white">OrderSiteBar</aside>;
 }
