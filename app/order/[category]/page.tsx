331850f3cc5be913d8858edcb230fce61a0fec88
Fernando <fernando.inversionmf@gmail.com>
2025-02-07 21:58:28 -0800

    Getting the products, iterating over them and highlighting the current category

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,9 +1,35 @@
-export default function OrderPage({
+import { prisma } from "@/src/lib/prisma";
+import ProductCard from "@/components/products/ProductCard";
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+  return products;
+}
+
+export default async function OrderPage({
   params,
 }: {
   params: { category: string };
 }) {
   console.log(params.category);
 
-  return <div>orderPage</div>;
+  const products = await getProducts(params.category);
+
+  return (
+    <>
+      <h1 className="text-2xl my-10">Select the order below:</h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 }
